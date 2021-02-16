b0ae7378e33f08b7bc32690e63cb44987d2593b6
Christian Alpoim <chrufes@yahoo.com.br>
2021-02-16 10:44:38 -0300

    Aula 09 - Criando o componente Grid com Styled Components

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Menu from "../src/components/commons/Menu";
 import Footer from "../src/components/commons/Footer";
 import Text from "../src/components/foundation/Text";
 import { Button } from "../src/components/commons/Button";
+import { Grid } from "../src/components/foundation/layout/Grid";
 
 export default function Home() {
   return (
@@ -16,36 +17,47 @@ export default function Home() {
     >
       <Menu />
 
-      <div>
-        <Text
-          variant="title"
-          tag="h1"
-          color="tertiary.main"
-          textAlign={{ sm: "center", md: "left" }}
-        >
-          Compartilhe momentos e conecte-se com amigos
-        </Text>
+      <Grid.Container>
+        <Grid.Row>
+          <Grid.Col offset={{ xs: 0, md: 1 }} value={{ xs: 12, md: 5 }}>
+            <Text
+              variant="title"
+              tag="h1"
+              color="tertiary.main"
+              textAlign={{ sm: "center", md: "left" }}
+            >
+              Compartilhe momentos e conecte-se com amigos
+            </Text>
 
-        <Text
-          variant="paragraph1"
-          tag="p"
-          color="tertiary.light"
-          textAlign={{ sm: "center", md: "left" }}
-        >
-          Lorem Ipsum lorem ipsum lorem ipsum
-        </Text>
+            <Text
+              variant="paragraph1"
+              tag="p"
+              color="tertiary.light"
+              textAlign={{ sm: "center", md: "left" }}
+            >
+              Lorem Ipsum lorem ipsum lorem ipsum
+            </Text>
 
-        <Button
-          margin={{
-            xs: "auto",
-            md: "initial",
-          }}
-          display="block"
-          variant="primary.main"
-        >
-          Cadastrar
-        </Button>
-      </div>
+            <Button
+              margin={{
+                xs: "auto",
+                md: "initial",
+              }}
+              display="block"
+              variant="primary.main"
+            >
+              Cadastrar
+            </Button>
+          </Grid.Col>
+          <Grid.Col value={{ xs: 12, md: 6 }}>
+            <img
+              style={{ display: "block", margin: "auto" }}
+              src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
+            />
+          </Grid.Col>
+        </Grid.Row>
+      </Grid.Container>
+
       <Footer />
     </div>
   );
